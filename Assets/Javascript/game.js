0514849a05e0ed2d5f264928dc339aae9374190a
Green Man <brendan.mccaughey@gmail.com>
2018-09-30 21:53:09 -0400

    Preliminary notes and HTML. Starting to define variables.

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,8 @@
+//Define variables here.
+var wordbank = ["","","","",""];
+
+var guessesRemain = 13;
+
+var userGuess = event.key;
+
+var wordChoice = wordbank[Math.floor(Math.random() * wordbank.length)];
